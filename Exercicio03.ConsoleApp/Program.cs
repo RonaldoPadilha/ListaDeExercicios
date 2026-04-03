85a80d0cae90505943f4463e9caa3e8124154a61
Ronaldo Padilha <ronaldocp762@gmail.com>
2026-04-03 19:46:00 -0300

    Adicionado exercício 03

---
+++ Exercicio03.ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿// 3. Crie um programa para converter a temperatura da escala Celsius para a escala Fahrenheit.
+// Multiplique a temperatura em Celsius por 1,8 e some 32 ao resultado para obter o valor em Fahrenheit.
+// (Fórmula): F = C * 1,8 + 32
+
+Console.Clear();
+Console.WriteLine("------- Conversor de Celsius para Fahrenheit -------");
+Console.WriteLine();
+Console.WriteLine("Digite a temperatura em °C");
+double celsius = Convert.ToDouble(Console.ReadLine());
+
+double fahrenheit = celsius * 1.8 + 32;
+Console.WriteLine();
+Console.WriteLine("A conversão é: " + fahrenheit + " °F");
+
+Console.ReadLine();
