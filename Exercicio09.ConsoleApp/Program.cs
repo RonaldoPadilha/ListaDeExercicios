e62c16d0d00131e52d5add05731ce3d8cda7d01c
Ronaldo Padilha <ronaldocp762@gmail.com>
2026-04-06 11:53:26 -0300

    Adicionado exercício 09

---
+++ Exercicio09.ConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿/* 9. Escreva um algoritmo que leia um valor inicial A e imprima a sequência de valores do cálculo de A! e o seu resultado.
+    a. Ex: 5! = 5 X 4 X 3 X 2 X 1 = 120
+    b. Pesquise sobre “fatorial”
+*/
+//Console.Clear();
+Console.WriteLine("---- Fatotial ----");
+
+Console.WriteLine("Digite um número: ");
+double numero = Convert.ToDouble(Console.ReadLine());
+double resultado = 1;
+
+Console.Write($" {numero}! = ");
+
+for (double contador = numero; contador >= 1; contador--)
+{
+    resultado = resultado * contador;
+
+    if (contador > 1)
+    {
+        Console.Write($"{contador} X ");
+    }
+    else
+    {
+        Console.Write($"{contador} = {resultado}");
+    }
+}
+
+Console.ReadLine();
